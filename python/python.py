89d561bacc4cb69d5e18c95ed835d1e473f72fa3
steventhomasdev <steventhomasdev@gmail.com>
2021-01-31 00:23:32 +0530

    added new file

---
+++ python/python.py
@@ -0,0 +1,16 @@
+
+from typing import Sequence
+
+
+class point():
+    def __init__(self,x,y):
+            self.y = y
+            self.x = x
+    def value (self):
+        print(f"the value of {self.x} * {self.y} = {self.x * self.y}")
+
+
+
+object = point(int(input('X : ')),int(input('X : ')))
+object.value()
+
